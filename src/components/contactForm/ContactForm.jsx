b215983903646551e577b030f787f93ed837a64f
ivelceva <73798374+ivelceva@users.noreply.github.com>
2023-06-18 12:10:31 -0700

    Add_fix

--- src/components/contactForm/ContactForm.jsx
+++ src/components/contactForm/ContactForm.jsx
@@ -2,33 +2,33 @@ import React, { useState } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import { nanoid } from 'nanoid';
 import { getContacts } from 'redux/selectors';
-//import { addContact } from '../../redux/contactSlice';
+import { addContact } from '../../redux/contactSlice';
 import css from './ContactForm.module.css';
 
 const ContactForm = () => {
-const dispatch = useDispatch();
-const contacts = useSelector(getContacts);
+  const dispatch = useDispatch();
+  const contacts = useSelector(getContacts);
 
-const [name, setName] = useState('');
-const [number, setNumber] = useState('');
+  const [name, setName] = useState('');
+  const [number, setNumber] = useState('');
 
-const handleChange = event => {
-  const { name, value } = event.target;
-  if (name === 'name') {
-    setName(value);
-  } else if (name === 'number') {
-    setNumber(value);
-  }
-};
+  const handleChange = event => {
+    const { name, value } = event.target;
+    if (name === 'name') {
+      setName(value);
+    } else if (name === 'number') {
+      setNumber(value);
+    }
+  };
 
   const handleSubmit = event => {
     event.preventDefault();
-    addContact(name, number);
+    addNewContact(name, number);
     setName('');
     setNumber('');
   };
 
-  const addContact = (name, number) => {
+  const addNewContact = (name, number) => {
     let result = contacts.find(item => item.name === name);
     if (result) {
       return alert(`${name} is already in contacts`);
@@ -37,27 +37,11 @@ const handleChange = event => {
     dispatch(addContact(newUser));
   };
 
-
-
-// const handleSubmit = event => {
-//   event.preventDefault();
-
-//   const result = contacts.find((contact) => contact.name === name);
-//   if (result) {
-//     alert(`${name} is already in contacts`);
-//     setName('');
-//   } else {
-//     dispatch(addContact({ name, number }));
-//     setName('');
-//     setNumber('');
-//   }
-// };
-
-return (
-<form className={css.form} onSubmit={handleSubmit}>
-  <label className={css.label}>
-    <p>Name</p>
-      <input
+  return (
+    <form className={css.form} onSubmit={handleSubmit}>
+      <label className={css.label}>
+        <p>Name</p>
+        <input
           name="name"
           type="text"
           placeholder="your name..."
@@ -66,33 +50,31 @@ return (
           pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
           title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
           required
-     />
-  </label>
+        />
+      </label>
 
-<label className={css.label}>
-  <p>Number</p>
-    <input
-       name="number"
-       type="tel"
-       placeholder="add valid number..."
-       value={number}
-       onChange={handleChange}
-       pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
-       title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
-       required
-     />
-</label>
+      <label className={css.label}>
+        <p>Number</p>
+        <input
+          name="number"
+          type="tel"
+          placeholder="add valid number..."
+          value={number}
+          onChange={handleChange}
+          pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
+          title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
+          required
+        />
+      </label>
 
-  <button className={css.btn} type="submit">
-    Add contact
-  </button>
-</form>
-);
+      <button className={css.btn} type="submit">
+        Add contact
+      </button>
+    </form>
+  );
 };
 
 export default ContactForm;
-
-
 
 // import React, { useState } from 'react';
 // import PropTypes from 'prop-types';
@@ -168,4 +150,3 @@ export default ContactForm;
 // };
 
 // export default ContactForm;
-
